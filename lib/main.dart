eb2474ca05479062ec2d87a03cdb9f19fbef218c
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-05-21 10:57:09 +0530

    295. PlaceList and Place Provider setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:great_places_app/providers/great_places.dart';
+import 'package:great_places_app/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(primarySwatch: Colors.indigo, accentColor: Colors.amber),
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello'),
-        ),
+    return ChangeNotifierProvider(
+      create: (context) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme:
+            ThemeData(primarySwatch: Colors.indigo, accentColor: Colors.amber),
+        home: PlacesListScreen(),
       ),
     );
   }
