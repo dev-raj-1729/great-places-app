78cb7809afdb180ea3f9ca6b7d3faf4b5ad16a00
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-05-22 10:15:24 +0530

    305. Preparing SQLite

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:great_places_app/helpers/db_helper.dart';
 import 'dart:io';
 
 import 'package:great_places_app/models/place.dart';
@@ -22,5 +23,13 @@ class GreatPlaces with ChangeNotifier {
     );
     _items.add(newPlace);
     notifyListeners();
+    DBHelper.insert(
+      'places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path
+      },
+    );
   }
 }
